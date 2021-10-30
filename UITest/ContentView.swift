7e894e97bba0a0a13ab9db15d80dd1fd4fc49781
Ayodeji <ayodejiayankola@gmail.com>
2021-10-30 20:48:32 +0100

    Working with vstack,Hstack, Zstack and spacer

--- UITest/ContentView.swift
+++ UITest/ContentView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello!"/*@END_MENU_TOKEN@*/)
-            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            .background(Color.green
-                            .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
-            
+        VStack {
+            Spacer()
+            Image("logo")
+                .resizable()
+                .aspectRatio( contentMode:
+                    .fit)
+            Spacer()
+            HStack(spacing: 28.0) {
+                Spacer()
+                Text("Hello")
+                Spacer()
+                Text("World!")
+                Spacer()
+               
+            }
+            Spacer()
+        }
+        
     }
 }
 
